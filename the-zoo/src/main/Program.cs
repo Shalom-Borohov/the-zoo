b80ef3a896020e6bf47e003dd99734a34b6490fb
Shalom Borohov <shalom842633@gmail.com>
2023-06-26 09:32:51 +0300

    Remove commentted out lines

--- the-zoo/src/main/Program.cs
+++ the-zoo/src/main/Program.cs
@@ -1,18 +1,5 @@
 ﻿using the_zoo.src.main;
 
 var theZoo = new TheZoo();
+
 theZoo.Start();
-// List<List<(string, object)>> animalsKeyValuePairsLists = animalsKeyValuePairsEnumerable.ToList();
-
-
-//var jsonFormatter = new JsonFormatter();
-//var csvFormatter = new CsvFormatter();
-
-//string json = jsonFormatter.Format(animalsKeyValuePairsLists);
-//string csv = csvFormatter.Format(animalsKeyValuePairsLists);
-
-//var jsonSerializer = new JsonSerializer();
-//var csvSerializer = new CsvSerializer();
-
-//jsonSerializer.SerializeToFile("animals.json", json);
-//csvSerializer.SerializeToFile("animals.csv", csv);
